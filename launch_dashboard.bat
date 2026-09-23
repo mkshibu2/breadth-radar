e18b3333bc620d650febab237ecf37aeb1066e77
Shibu M K <shibu.mk@gmail.com>
2026-09-23 12:41:38 +0530

    update

--- launch_dashboard.bat
+++ launch_dashboard.bat
@@ -3,6 +3,6 @@ title NSE Breadth Radar Launcher
 echo Starting Unified CORS Local Proxy in background...
 start "" /min node proxy.js
 echo Opening Dashboard in browser...
-[start index.html]
+start "" index.html
 echo Startup complete.
 exit
